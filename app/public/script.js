63392590fdeb773924ab35eb16bb2f9bc743dbee
Aaron DeVera <aaronsdevera@gmail.com>
2014-09-21 02:03:27 -0400

    update 02:00am

--- app/public/script.js
+++ app/public/script.js
@@ -3,19 +3,24 @@
 $(document).ready(function(){
 	$(".card").hide();
 	var openCard = false;
-	
+
+
+
 	$("#ticker").click(function(){
 		if (openCard==false){
 		$(".card").fadeIn(100);
 			openCard = true;
 			console.log("Card open (openCard=" + openCard +")");
-
+			$("#body_container").addClass("makeBlur");
 		}
 		else if (openCard==true){
 			$(".card").fadeOut(100);
 			openCard = false;
 			console.log("Card closed (openCard=" + openCard +")");
+			$("#body_container").removeClass("makeBlur");
 		}
 	});
+
+
 
 });
