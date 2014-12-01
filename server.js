eec282eca2483ca8b7dbe73d079499344c3406b9
root <root@durkin.info>
2014-12-01 04:05:53 +0000

    optimized for apache hosting

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ var app = express();
 var port = 4000;
 app.listen(port);
 
-
-
-
-
-
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
@@ -15,9 +10,7 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
-app.use(express.static(__dirname + '/public'));
-
-
+app.use(express.static(__dirname));// + '/public'));
 
 console.log("Listening on port " + port);
 
